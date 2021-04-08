f49c19d6beb95c53cdb6d3023a04ae600f2f3667
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2021-04-08 16:22:58 +0200

    live api

---
+++ Manager/SchoolManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Manager;
+
+use App\Classes\DB;
+use App\Entity\School;
+
+class SchoolManager {
+
+    /**
+     * Return a school based on id.
+     * @param int $id
+     * @return School
+     */
+    public function getSchool(int $id): School {
+        $request = DB::getInstance()->prepare("SELECT * FROM school WHERE id=:id");
+        $request->bindValue(':id', $id);
+        $request->execute();
+        $school_data = $request->fetch();
+        $school = new School();
+        if ($school_data) {
+            $school->setId($school_data['id']);
+            $school->setName($school_data['name']);
+        }
+        return $school;
+    }
+}
